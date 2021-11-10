0c952bc3fc54ad0a2494a46a8b2faf083f6938f8
Fabian Baldenweg <bafabian@ethz.ch>
2021-11-10 16:47:31 +0100

    added keras wrapper and MlpRegressorSetup with option to use grid search to find best hyperparameters

--- Fabian.py
+++ Fabian.py
@@ -37,7 +37,7 @@ except ImportError or ModuleNotFoundError:
 
 
 folds = 10
-useTestResultsFromCrossValidationEnsemble = True
+useTestResultsFromCrossValidationEnsemble = False
 
 def initialImputation(X):
     imp = SimpleImputer(missing_values=np.nan, strategy='median')
@@ -58,6 +58,14 @@ def outlier_detection(X, y, indicatorMat = None): #use isolation forest
     else:
         return X[outlierResults == 1], y[outlierResults == 1], indicatorMat[outlierResults == 1]
 
+def isolationForestOutlierDetection(X, contamination=0.05, n_estimators=200, max_samples="auto", bootstrap=False):
+    """
+    Returns a mask where the inliers are 1 and the outliers are 0.
+    """
+    isoForest = IsolationForest(n_estimators=n_estimators, max_samples=max_samples, bootstrap=bootstrap, contamination=contamination)
+    outlierResults = isoForest.fit_predict(X)
+    print(f"number of outliers: {(outlierResults != 1).sum()} outliers in {X.shape[0]} samples")
+    return outlierResults == 1
 
 def feature_selection(X_test, X_train, y_train, missingValsTrain=None, missingValsTest=None): #missingValsTrain and missingValsTest can be used to remember where there were missing features in the input data (i.e. the same columns are selected on these ndarrays)
     l1_lambda = 0.2
@@ -87,6 +95,34 @@ def feature_selection(X_test, X_train, y_train, missingValsTrain=None, missingVa
     else:
         raise NotImplementedError("missingValsTrain and missingValsTest must either be both given or both None")
 
+def lassoFeatureSelection(X_train, y_train, l1_lambda):
+    """
+    Returns a list of the indices of the selected features.
+    """
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    #print(f"means: {scaler.mean_}")
+    normalizedX_train = scaler.transform(X_train)
+    lasso = Lasso(alpha=l1_lambda).fit(normalizedX_train, y_train)
+    selectionModel = SelectFromModel(lasso, prefit=True)
+    lassoFilteredIdxs = selectionModel.get_support(indices=True)
+    return lassoFilteredIdxs
+
+def fRegressionFeatureSelection(X_train, y_train, numFeatures):
+    """
+    Returns a list of the indices of the selected features.
+    """
+    f_test, _ = f_regression(X_train, y_train,center=False)
+    f_test /= np.max(f_test)
+    f_test_idx = f_test.argsort()[-numFeatures:][::-1]
+    return f_test_idx
+
+def mutualInfoRegressionFeatureSelection(X_train, y_train, numFeatures, nNeighbors=5):
+    mi = mutual_info_regression(X_train, y_train,n_neighbors=nNeighbors)
+    mi /= np.max(mi)
+    mi_idx = mi.argsort()[-numFeatures:][::-1]
+    return mi_idx
+
 def feature_selection_matt(X_test, X_train, y_train): #feature selection from Matt; using to for ensemble with feature selection from lasso
     C = 400
     f_test, _ = f_regression(X_train, y_train,center=False)
@@ -103,10 +139,25 @@ def feature_selection_matt(X_test, X_train, y_train): #feature selection from Ma
     #return X_test[:,idx_filtered], X_train[:,idx_filtered]
     return idx_filtered #modified to return indices instead of filtered data
 
+def intersect(*args):
+    if len(args) == 1:
+        return args[0]
+    I = np.intersect1d(args[0], args[1])
+    for i in range(2, len(args)):
+        I = np.intersect1d(args[i], I)
+    return I
 
 def missing_values(X): #imputing missing values
     #imp = IterativeImputer(n_nearest_features=200, initial_strategy='mean')
     imp = sklearn.impute.KNNImputer(n_neighbors=30, missing_values=np.nan)
+    return imp.fit_transform(X)
+
+def impute_KNN(X, n_neighbors=30):
+    imp = sklearn.impute.KNNImputer(n_neighbors=n_neighbors, missing_values=np.nan)
+    return imp.fit_transform(X)
+
+def impute_MICE(X, n_nearest_features=200, initial_strategy='mean'):
+    imp = IterativeImputer(n_nearest_features=n_nearest_features, initial_strategy=initial_strategy)
     return imp.fit_transform(X)
 
 def rescale(X_test, X_train):
@@ -136,16 +187,19 @@ def det_coeff(y_true, y_pred):
     SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
     return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 
-class BaseRegressorWrapper:
+class BaseRegressorWrapper(sklearn.base.BaseEstimator):
     """
     Class to allow RegressorSetup to be used with models that are not from sklearn. Implement a subclass as a wrapper around a model from another ML library.
     """
     @abstractmethod
     def fit(self, X, y):
-        pass
+        return self
     @abstractmethod
     def predict(self, X):
         pass
+
+    def score(self, X, y):
+        return r2_score(y, self.predict(X))
 
     @abstractmethod
     def getActualRegressor(self):
@@ -157,9 +211,9 @@ class BaseRegressorWrapper:
 class KerasWrapper(BaseRegressorWrapper):
     def __init__(self, kerasModel: tf.keras.Model, nEpochs, batch_size=None, verbose="auto", callbacks=None, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None, sample_weight=None, initial_epoch=0, steps_per_epoch=None, validation_steps=None, validation_batch_size=None, validation_freq=1, max_queue_size=10, workers=1, use_multiprocessing=False
 ):
-        self.model = model
+        self.model = kerasModel
         self.batch_size=batch_size
-        self.epochs=nEpochs
+        self.nEpochs=nEpochs
         self.verbose=verbose
         self.callbacks=callbacks
         self.validation_split=validation_split
@@ -178,14 +232,20 @@ class KerasWrapper(BaseRegressorWrapper):
 
         self.history = None
 
-    def fit(self, X, y):
-        self.history = self.model.fit(X, y, batch_size=self.batch_size, epochs=self.epochs, verbose=self.verbose, callbacks=self.callbacks, validation_split=self.validation_split, validation_data=self.validation_data, shuffle=self.shuffle, class_weight=self.class_weight, sample_weight=self.sample_weight, initial_epoch=self.initial_epoch, steps_per_epoch=self.steps_per_epoch, validation_steps=self.validation_steps, validation_batch_size=self.validation_batch_size, validation_freq=self.validation_freq, max_queue_size=self.max_queue_size, workers=self.workers, use_multiprocessing=self.use_multiprocessing)
+    def fit(self, X, y, validation_split = None, validation_data = None):
+        if validation_split is None:
+            validation_split = self.validation_split
+        if validation_data is None:
+            validation_data = self.validation_data
+        self.history = self.model.fit(X, y, batch_size=self.batch_size, epochs=self.nEpochs, verbose=self.verbose, callbacks=self.callbacks, validation_split=validation_split, validation_data=validation_data, shuffle=self.shuffle, class_weight=self.class_weight, sample_weight=self.sample_weight, initial_epoch=self.initial_epoch, steps_per_epoch=self.steps_per_epoch, validation_steps=self.validation_steps, validation_batch_size=self.validation_batch_size, validation_freq=self.validation_freq, max_queue_size=self.max_queue_size, workers=self.workers, use_multiprocessing=self.use_multiprocessing)
+        return self
 
     def predict(self, X):
         return self.model.predict(X)
 
-    def displayAdditionalInformation():
+    def displayAdditionalInformation(self):
         if self.history is not None:
+            print(f"History: {self.history.history.items()}")
             if 'val_det_coeff' in self.history.history.items():
                 plt.subplot(1, 2, 2)
                 plt.plot(self.history.history['val_det_coeff'], color='orange', label='val score')
@@ -199,32 +259,38 @@ class BaseRegressorSetup: #Base class for regressors; constructor initializes in
 
     def __init__(self, X_train, Y_train, X_test): #X_train and X_test are assumed to be unmodified (i.e. directly read from the csv)
         self.paramDescription = ""
+        self.prepareDatasets(X_train, Y_train, X_test)
+
+
+    def prepareDatasets(self, X_train, Y_train, X_test, basicImputationForOutlierDetection=True, normalize=True):
+        missingTrainEntries = np.isnan(X_train)
+        missingTestEntries = np.isnan(X_test)
         
-        missingTrainEntries = X_train == np.nan
-        missingTestEntries = X_test == np.nan
-        
-        X_train = initialImputation(X_train)
-        X_test = initialImputation(X_test)
+        if basicImputationForOutlierDetection:
+            X_train = initialImputation(X_train)
+            X_test = initialImputation(X_test)
         
         # remove outliers
-        X_train, Y_train, missingTrainEntries = outlier_detection(X_train, Y_train, missingTrainEntries)
+        X_train, Y_train, missingTrainEntries = self.detectOutliers(X_train, Y_train, missingTrainEntries)
         
-        X_test, X_train = rescale(X_test, X_train)
+        if normalize:
+            X_test, X_train = rescale(X_test, X_train)
         
         X_train[missingTrainEntries] = np.nan
         X_test[missingTestEntries] = np.nan
         
         # fill in missing values
-        X_test = missing_values(X_test)
-        X_train = missing_values(X_train)
-        
+        X_train, X_test = self.imputeMissing(X_train, X_test)
         
         Y_train = Y_train.ravel()
         
-        X_test, X_train = feature_selection(X_test, X_train, Y_train)
+        X_train, X_test = self.selectFeatures(X_train, Y_train, X_test)
         self.X_train = X_train
         self.Y_train = Y_train
         self.X_test = X_test
+
+    def shuffleTrainingData(self):
+        self.X_train, self.Y_train = sklearn.utils.shuffle(self.X_train, self.Y_train)
 
     def getRegressor(self):
         raise NotImplementedError("getRegressor needs to be implemented in a subclass")
@@ -238,6 +304,9 @@ class BaseRegressorSetup: #Base class for regressors; constructor initializes in
 
         return self.getRegressor().fit(X_train, Y_train)
 
+    def getParamDescription(self):
+        return self.paramDescription
+
     def crossValidate(self, nFolds, score='r2', shuffle=False):
         if shuffle:
             X_train, Y_train = sklearn.utils.shuffle(self.X_train, self.Y_train)
@@ -245,6 +314,25 @@ class BaseRegressorSetup: #Base class for regressors; constructor initializes in
             X_train = self.X_train
             Y_train = self.Y_train
         return sklearn.model_selection.cross_validate(self.getRegressor(), X_train, y=Y_train, scoring=score, cv=nFolds, return_train_score=True)
+
+    @classmethod
+    def selectFeatures(cls, X_train, Y_train, X_test):
+        selectedFeatures = intersect(lassoFeatureSelection(X_train, Y_train, l1_lambda=0.2), fRegressionFeatureSelection(X_train, Y_train, numFeatures=400), mutualInfoRegressionFeatureSelection(X_train, Y_train, numFeatures=400))
+        return X_train[:, selectedFeatures], X_test[:, selectedFeatures]
+
+    @classmethod
+    def detectOutliers(cls, X_train, Y_train, indicatorMat = None):
+        mask = isolationForestOutlierDetection(X_train)
+        if indicatorMat is None:
+            return X_train[mask], Y_train[mask]
+        else:
+            return X_train[mask], Y_train[mask], indicatorMat[mask]
+
+    @classmethod
+    def imputeMissing(cls, X_train, X_test):
+        return impute_KNN(X_train), impute_KNN(X_test)
+
+
 
 class ElasticNetRegressorSetup(BaseRegressorSetup):
 
@@ -257,8 +345,8 @@ class ElasticNetRegressorSetup(BaseRegressorSetup):
 class GradientBoostingRegressorSetup(BaseRegressorSetup):
 
     def __init__(self, X_train, Y_train, X_test): #X_train and X_test are assumed to be unmodified (i.e. directly read from the csv)
-        missingTrainEntries = X_train == np.nan
-        missingTestEntries = X_test == np.nan
+        missingTrainEntries = np.isnan(X_train)
+        missingTestEntries = np.isnan(X_test)
         
         X_train = initialImputation(X_train)
         X_test = initialImputation(X_test)
@@ -308,8 +396,96 @@ class RandomForestRegressorSetup(BaseRegressorSetup):
         regr = sklearn.ensemble.RandomForestRegressor(n_estimators=self.nTrees, max_depth=self.maxDepth, min_samples_split=self.minSamplesSplit, bootstrap=self.bootstrap, max_features=self.maxFeatures, max_leaf_nodes=self.maxLeafNodes, n_jobs=-1)
         return regr
 
+DEFAULT_N_EPOCHS = 50
+class MlpRegressor(KerasWrapper):
+
+    def __init__(self, inputShape, hiddenLayerSizes=[10], dropout=0.5, nEpochs=DEFAULT_N_EPOCHS):
+        self.inputShape = inputShape
+        self.hiddenLayerSizes = hiddenLayerSizes
+        self.dropout=dropout
+        model = self.getNewModel()
+        super().__init__(model, nEpochs=nEpochs)
+    
+    def getNewModel(self):
+        #x = keras.Input(shape=self.X_train.shape[1:])
+        x = keras.Input(shape=self.inputShape)
+        y = x
+        for hiddSz in self.hiddenLayerSizes:
+            y = layers.Dense(hiddSz, activation='relu')(y)
+            y = layers.Dropout(self.dropout)(y)
+        y = layers.Dense(1)(y)
+        model = keras.Model(inputs=x, outputs=y)
+        model.compile(loss="mean_squared_error", optimizer='adam', metrics=[det_coeff])
+        #model.summary()
+        return model
+
+    def set_params(self, inputShape=None, hiddenLayerSizes=[10], dropout=0.5, nEpochs=None):
+        self.hiddenLayerSizes = hiddenLayerSizes
+        self.dropout=dropout
+        if nEpochs is not None:
+            self.nEpochs = nEpochs
+        if inputShape is not None:
+            self.inputShape = inputShape
+        self.model = self.getNewModel()
+        return self
+
+    def reset(self):
+        self.model = self.getNewModel()
+        return self
+
+    def get_params(self, deep=True):
+        return {
+            "hiddenLayerSizes": self.hiddenLayerSizes,
+            "dropout": self.dropout,
+            "nEpochs": self.nEpochs,
+            "inputShape": self.inputShape
+        }
+
+
+#class MlpRegressorSetup(BaseRegressorSetup, sklearn.base.BaseEstimator, KerasWrapper):
 class MlpRegressorSetup(BaseRegressorSetup):
-    pass #TODO
+    
+
+    def __init__(self, X_train, Y_train, X_test, hiddenLayerSizes=[10], dropout=0.5, nEpochs=DEFAULT_N_EPOCHS):
+        self.prepareDatasets(X_train, Y_train, X_test)
+        self.setParams(hiddenLayerSizes=hiddenLayerSizes, dropout=dropout, nEpochs=nEpochs)
+        #model = self.getNewModel()
+        #KerasWrapper.__init__(self, model, nEpochs=nEpochs)
+
+    def setParams(self, hiddenLayerSizes=[10], dropout=0.5, nEpochs=DEFAULT_N_EPOCHS, **kwargs): #**kwargs is for accepting parameters from MLPRegressor
+
+        #self.hiddenLayerSizes = hiddenLayerSizes
+        #self.dropout=dropout
+        #self.nEpochs = nEpochs
+        self.model = MlpRegressor(inputShape=self.X_train.shape[1:], hiddenLayerSizes=hiddenLayerSizes, dropout=dropout, nEpochs=nEpochs)
+        #self.model = self.getNewModel()
+    
+    def getParamDescription(self):
+        def numbersToString(nrs, separator="-"):
+            s = ""
+            for n in nrs:
+                s += str(n) + separator
+            return s[:-1]
+        return "hiddenLayerSizes_" + numbersToString(self.model.hiddenLayerSizes) + "_dropout_" + str(self.model.dropout)
+
+
+    def getRegressor(self):
+        #first reset model
+        return self.model.reset()
+
+
+
+    def findBestParams(self, nFolds=10, paramGrid = {"hiddenLayerSizes": [[10], [20, 10]], "dropout": [0.25, 0.3, 0.4, 0.5]}):
+        """
+        Finds the best parameter combination by exhaustive search; k-fold cross validation is used to determine the performance of each parameter combination.
+        After completion, this functions sets the parameters of this RegressorSetup to the best combination and prints this combination.
+        """
+        gscv = sklearn.model_selection.GridSearchCV(estimator=self.model, param_grid=paramGrid, n_jobs=1, cv=nFolds)
+        gscv.fit(self.X_train, self.Y_train)
+        self.model.set_params(**gscv.best_params_)
+        print("best parameter combination found: " + str(gscv.best_params_))
+        return gscv.cv_results_
+
 
 def testResultsFromCrossValidationEnsembleSingleRegressor(regSetup : BaseRegressorSetup, nFolds, shuffle=True):
     """
@@ -367,15 +543,16 @@ def crossValidateEnsemble(regSetups, nFolds: int, returnCrossValidationEnsembleR
             regr = regSetup.getRegressor()
             regr.fit(xTrain, yTrain)
             yValPred = regr.predict(xVal)
+            
+            weight = max(r2_score(yVal, yValPred), 0)
 
+            yValNormalization += weight
+            yValPredTotal += yValPred * weight
 
-            yValNormalization += 1
-            yValPredTotal += yValPred
+            yTrainTotal += regr.predict(xTrain) * weight
 
-            yTrainTotal += regr.predict(xTrain)
-
-            yTestNormCurSplit += 1
-            yTestPredCurSplit += regr.predict(xTest)
+            yTestNormCurSplit += weight
+            yTestPredCurSplit += regr.predict(xTest) * weight
         
         yTrainTotal /= yValNormalization
         yValPredTotal /= yValNormalization
@@ -470,21 +647,28 @@ y = read_csv('y_train.csv')
 regSetup = GradientBoostingRegressorSetup(X_train, y, X_test)
 #regSetup = RandomForestRegressorSetup(X_train, y, X_test)
 
+#regSetup = MlpRegressorSetup(X_train, y, X_test, hiddenLayerSizes=[20, 10], dropout=0.25, nEpochs=300)
+#regSetup.shuffleTrainingData()
+#regr = regSetup.getRegressor()
+#regr.fit(regSetup.X_train, regSetup.Y_train, validation_split=0.2)
+##print(regSetup.findBestParams())
+#regr.displayAdditionalInformation()
+
 if "ensemble" in vars() and len(ensemble) > 1:
     if not useTestResultsFromCrossValidationEnsemble:
         print("Note: useTestResultsFromCrossValidationEnsemble is False, but ensemble is defined, so a cross validation ensemble is used anyway")
     yPred, crossValResults = crossValidateEnsemble(ensemble, nFolds=folds, returnCrossValidationEnsembleResults=True, shuffle=True)
     testOut = f"subfab_ensemble_{folds}foldCV"
     for regSetup in ensemble:
-        testOut += "-" + type(regSetup).__name__ + "_" + regSetup.paramDescription
+        testOut += "-" + type(regSetup).__name__ + "_" + regSetup.getParamDescription()
     testOut += ".csv"
 else:
     if useTestResultsFromCrossValidationEnsemble:
         yPred, crossValResults = testResultsFromCrossValidationEnsembleSingleRegressor(regSetup, nFolds=folds)
-        testOut = 'subfab_' + str(folds) + "foldCvEnsemble_" + type(regSetup).__name__ + "_" + regSetup.paramDescription + '.csv'
+        testOut = 'subfab_' + str(folds) + "foldCvEnsemble_" + type(regSetup).__name__ + "_" + regSetup.getParamDescription() + '.csv'
     else:
         crossValResults = regSetup.crossValidate(nFolds=folds, shuffle=True)
-        testOut = 'subfab_' + type(regSetup).__name__ + "_" + regSetup.paramDescription + '.csv'
+        testOut = 'subfab_' + type(regSetup).__name__ + "_" + regSetup.getParamDescription() + '.csv'
 
 plt.title("cross validation scores (coefficients of determination)")
 plt.xlabel("fold number")
