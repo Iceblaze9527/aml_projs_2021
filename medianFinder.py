2bd8ea01d9f9fd55f43a4b8123903249d3ad3ca6
Iceblaze9527 <iceblaze9527@85-195-199-32.init7.net>
2022-01-05 13:05:29 +0100

    commit to private repo

--- medianFinder.py
+++ medianFinder.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 
 #filenames = ["y_test_yutong_v1.csv", "y_test_yutong_v2.csv", "y_test_yutong_v3.csv", "y_test_yutong_v4.csv", "y_test_yutong_v5.csv", "y_test_yutong_v6.csv", "y_test_yutong_v7.csv", "y_test_yutong_v8.csv", "y_test_yutong_v9.csv"]
-filenames = ["y_test_yutong_v4.csv", "y_test_yutong_v5.csv", "y_test_yutong_v6.csv", "y_test_yutong_v7.csv", "y_test_yutong_v8.csv", "y_test_yutong_v9.csv"]
+# filenames = ["y_test_yutong_v4.csv", "y_test_yutong_v5.csv", "y_test_yutong_v6.csv", "y_test_yutong_v7.csv", "y_test_yutong_v8.csv", "y_test_yutong_v9.csv"]
+
+filenames = ["y_test_yutong_v4.csv", "y_test_yutong_v5.csv", "y_test_yutong_v6.csv", "y_test_yutong_v7.csv", "y_test_yutong_v8.csv", "y_test_yutong_v9.csv", "y_test_yutong_v14.csv", "y_test_yutong_v11.csv", "y_test_yutong_v12.csv", "y_test_yutong_v13.csv"]
 
 values = np.stack([pd.read_csv(filename).values[:, 1] for filename in filenames])
 print(values.shape)
@@ -11,4 +13,4 @@ print(np.median(values, axis=0).shape)
 ensembledValues = np.median(values, axis=0)
 
 outDf = pd.DataFrame({'y': ensembledValues})
-outDf.to_csv("ensembledValues.csv", index_label="id")
+outDf.to_csv("y_test_yutong_v16.csv", index_label="id")
